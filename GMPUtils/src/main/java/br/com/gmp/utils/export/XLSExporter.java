cfa0bc2161625e886d6fa3071b1a24bc915b042f
kaciano <kaciano@kaciano-K55A>
2014-02-04 23:14:46 -0200

    Exportação para XLS implementada.

--- GMPUtils/src/main/java/br/com/gmp/utils/export/XLSExporter.java
+++ GMPUtils/src/main/java/br/com/gmp/utils/export/XLSExporter.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jxl.Workbook;
-import jxl.format.CellFormat;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
